596d329ca8710949d80adcf96b539abf233dc421
alpsleonardo <alpsleonardo@gmail.com>
2016-11-07 11:04:38 -0500

    first commit

---
+++ app/src/main/java/com/algonquincollege/alve0024/doorsopenottawa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.algonquincollege.alve0024.doorsopenottawa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
