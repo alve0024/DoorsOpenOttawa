4361cb42d6543114469169f23eba21e4fe9dbf23
alpsleonardo <alpsleonardo@gmail.com>
2016-11-11 10:11:54 -0500

    refactor

---
+++ app/src/main/java/com/algonquincollege/alve0024/doorsopenottawa/HttpManager.java
@@ -0,0 +1,8 @@
+package com.algonquincollege.alve0024.doorsopenottawa;
+
+/**
+ * Created by leonardoalps on 2016-11-07.
+ */
+
+public class HttpManager {
+}
---
+++ app/src/main/java/com/algonquincollege/alve0024/doorsopenottawa/model/Building.java
@@ -0,0 +1,8 @@
+package com.algonquincollege.alve0024.doorsopenottawa.model;
+
+/**
+ * Created by leonardoalps on 2016-11-07.
+ */
+
+public class Building {
+}
